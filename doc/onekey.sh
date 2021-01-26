5921abc865c60d08698da5fac57846c9e3cabc82
Aliao <76611417+vod-ka@users.noreply.github.com>
2021-01-26 12:15:27 +0800

    Update onekey.sh

--- doc/onekey.sh
+++ doc/onekey.sh
@@ -33,7 +33,7 @@ Blue(){
 #更新yum源
 SourceUpdate(){
     mv -f /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup
-    #cd /etc/yum.repos.d/ || exit 0
+    cd /etc/yum.repos.d/ || exit 0
     curl -O http://mirrors.aliyun.com/repo/Centos-7.repo
     mv -f Centos-7.repo CentOS-Base.repo
     yum clean all
